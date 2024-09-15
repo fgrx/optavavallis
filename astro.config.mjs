67c47fe32942cebd00439590c68e5525bc8e71fe
Fabien Grignoux <fabien.grignoux@outlook.com>
2024-09-15 10:07:01 +0200

    Initialise astro tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+import tailwind from '@astrojs/tailwind';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind(), mdx()]
+});
